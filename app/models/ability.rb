b94303e18b6e03df4c08f40c31211b9eb586103b
AntonioHincapie <marcohincapiem@gmail.com>
2022-10-06 18:28:44 -0500

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    return unless (user)
+    return unless user
+
     can :manage, Category, author: user
     can :manage, Movement, author: user
-    end
   end
 end
