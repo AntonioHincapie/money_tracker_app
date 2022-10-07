0fd536b7f9f13d689bf10df63e22b967a87143ec
AntonioHincapie <marcohincapiem@gmail.com>
2022-10-07 12:42:56 -0500

    Seed database

--- db/migrate/20221006204943_create_clasifications.rb
+++ db/migrate/20221006204943_create_clasifications.rb
@@ -1,8 +1,8 @@
 class CreateClasifications < ActiveRecord::Migration[7.0]
   def change
     create_table :clasifications do |t|
-      t.references :movements, null: false, foreign_key: true
-      t.references :categories, null: false, foreign_key: true
+      t.belongs_to :movement, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
 
       t.timestamps
     end
