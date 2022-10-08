2b86c9584caec3b82640be225d25663d76ece8b5
AntonioHincapie <marcohincapiem@gmail.com>
2022-10-07 21:21:25 -0500

    Models specs

--- spec/models/movement_spec.rb
+++ spec/models/movement_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Movement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'Antonio', email: 'antonio@mail.com')
+    @user.password = 'pass1234'
+    @user.password_confirmation = 'pass1234'
+    @movement = Movement.create(name: 'Uber', amount: 15.67, user_id: @user.id)
+  end
+
+  context 'When testing Movement class' do
+    it 'Should have a name' do
+      expect(@movement.name).to eq('Uber')
+    end
+
+    it 'Should have a amount' do
+      expect(@movement.amount).to eq(15.67)
+    end
+
+    it 'Should have an author' do
+      expect(@movement.user_id).to eq(@user.id)
+    end
+  end
 end
